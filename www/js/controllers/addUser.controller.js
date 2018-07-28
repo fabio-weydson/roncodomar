d1019eaabda45a87df6554394883e885d75ffd40
Weydson <weydson@github.com>
2018-07-28 09:59:16 -0300

    mod3

--- www/js/controllers/addUser.controller.js
+++ www/js/controllers/addUser.controller.js
@@ -3,8 +3,6 @@
 app
 .controller('adduserCtrl', ['$scope', '$timeout', '$state', '$ionicLoading', 'dataservice',
   function($scope, $timeout, $state,  $ionicLoading, dataservice){
-    
- 
     $scope.$on('$ionicView.enter',function(){
      $ionicLoading.hide();
     });
--- www/js/controllers/cardapio.controller.js
+++ www/js/controllers/cardapio.controller.js
@@ -1,7 +1,7 @@
 /*global app */
 'use strict';
 app
-.controller('cardapioCtrl', ['$scope', '$stateParams', 'dataservice', 'appConfig', '$ionicLoading',,
+.controller('cardapioCtrl', ['$scope', '$stateParams', 'dataservice', 'appConfig', '$ionicLoading',
   function($scope, $stateParams, dataservice, appConfig , $ionicLoading){
       $scope.visivel = 1;
       $scope.exibe = function(param){
